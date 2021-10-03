17f464fe77dd022751e1f757ce20b692a2db9f77
root <root@DESKTOP-7ITP6GI.localdomain>
2021-10-02 20:26:47 -0500

    fixed random errors

--- cowSorting.java
+++ cowSorting.java
@@ -1,19 +1,23 @@
 import java.util.Scanner;
+import java.util.PrintWriter;
+import java.util.File;
 
 public class cowSorting {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int[] Array = new int[N];
-        for(int i = 0; i < N; i++){
-            Array[i] = sc.nextInt();
+    public static void main(String[] args) throws FileNotFoundException{
+        PrintWriter pw = new PrintWriter(new File("herding.out"));
+        Scanner sc = new Scanner(new File("herding.in"));
+        int[] cows = new int[3];
+        for(int i = 0; i < 3; i++){
+            cows[i] = sc.nextInt();
         }
-        int bruh = 0;
-        for(int i = Array.length - 1; i > 0; i--){
-            if(Array[i] == Array[i - 1] + 1){
-                bruh++;
-            }
+        Arrays.sort(cows);
+        if(cows[2] = cows[0] + 2){
+            pw.println(0);
+        }else if(cows[1] == cows[0] + 2 || cows[2] == cows[1] + 2){
+            pw.println(1);
+        }else{
+            pw.println(2);
         }
-        System.out.println(bruh);
+        pw.close();
     }
 }
